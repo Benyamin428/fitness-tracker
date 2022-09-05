9d8205c6a33e5d50dbaa01fab65125e6c4244af0
Benyamin428 <benyamin428@gmail.com>
2022-09-05 11:05:07 +0100

    Added CrossOrigin to ActivityController

--- backend/src/main/java/com/fitnesstracker/backend/controller/ActivityController.java
+++ backend/src/main/java/com/fitnesstracker/backend/controller/ActivityController.java
@@ -3,14 +3,12 @@ package com.fitnesstracker.backend.controller;
 import com.fitnesstracker.backend.model.Activity;
 import com.fitnesstracker.backend.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ActivityController {
 
     @Autowired
